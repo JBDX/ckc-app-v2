d6e3c785d797176ba707ddb2f3d9f67b85ac1ad1
JBDX <jbdx.dev@gmail.com>
2025-09-06 13:37:32 +0300

    knexfile update

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,6 +2,25 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+// Outil pour décomposer l'URL de la base de données
+const { parse } = require('pg-connection-string');
+
+// Par défaut, la connexion est l'URL simple
+let connection = process.env.DATABASE_URL;
+
+// Pour la production, nous décomposons l'URL et ajoutons la configuration SSL
+if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+  const dbConfig = parse(process.env.DATABASE_URL);
+  connection = {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    ssl: { rejectUnauthorized: false },
+  };
+}
+
 module.exports = {
   development: {
     client: 'pg',
@@ -15,10 +34,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false } // Ligne cruciale pour la connexion sur Railway
-    },
+    connection: connection, // On utilise notre objet de connexion dynamique
     migrations: {
       directory: './database/migrations'
     },
@@ -27,3 +43,4 @@ module.exports = {
     }
   }
 };
+
